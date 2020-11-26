db982f31d7bb63b3f1a4a1af27cda648dcf66a58
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-26 16:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import pyautogui
 import pyperclip
 import time
+
 text = pyperclip.paste()
-# for e in text:
-#     pyautogui.typewrite(e)
-#     time.sleep(.1) #change this value to whatever you mean by "a quick manner"
 
-
-
-pyautogui.write(text, interval=0.25)
+time.sleep(10)
+pyautogui.write(text, interval=0.20)
