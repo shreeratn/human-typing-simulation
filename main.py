9e597b47c384531d7d187aac3beb372875635adf
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-26 16:04:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pyautogui
 import pyperclip
 import time
-text= pyperclip.paste()
-for e in text: 
-    pyautogui.typewrite(e)
-    time.sleep(.1) #change this value to whatever you mean by "a quick manner"
+
+text = pyperclip.paste()
+# for e in text:
+#     pyautogui.typewrite(e)
+#     time.sleep(.1) #change this value to whatever you mean by "a quick manner"
+
+pyautogui.write("Hello world!", interval=0.25)
