9d106b5f13e03c753b6a3ea658b5a52b7962fb06
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-26 16:02:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyautogui
+import pyperclip
 import time
-text="what i want to type"
+text= pyperclip.paste()
 for e in text: 
     pyautogui.typewrite(e)
     time.sleep(.1) #change this value to whatever you mean by "a quick manner"
