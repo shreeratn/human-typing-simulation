4c01bd779b66f9a9d7d279605a8e5dd2c3971735
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-26 21:21:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ import time
 
 text = pyperclip.paste()
 
-# X = pyautogui.confirm(buttons=["Exit"])
-# if X == "Exit":
-#     exit()
-
-
 time_needed = int(
     pyautogui.prompt(
         """
         How much time(in seconds) do you need for
-        1. Minimizing this dialouge 
-        2. switching to the editor you want to simulate typing
-        3. Placing the cursor at starting postion
+        1. switching to the editor you want to simulate typing
+        2. Placing the cursor at starting postion
     """
     )
 )
