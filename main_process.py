6f2dbe7832c5024b99351bd42ea2c53ce9d4b1ed
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-27 10:09:42 +0530

    Update main_process.py

--- main_process.py
+++ main_process.py
@@ -1,8 +1,3 @@
 import subprocess
-import keyboard
 
-p = subprocess.Popen(['python','main.py'])
-
-key_stroke = keyboard.read_hotkey(suppress=True)
-if key_stroke == 'ctrl+esc':
-    p.terminate()
+p = subprocess.Popen(['python','main.py', 'key.py'])
