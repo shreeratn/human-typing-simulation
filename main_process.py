ce7fb4ae8cb486c5a2170ce08a5fb97fbd80fc55
Shree Ratn <shreeratn@users.noreply.github.com>
2020-11-27 10:28:19 +0530

    Update main_process.py

--- main_process.py
+++ main_process.py
@@ -1,3 +1,5 @@
 import subprocess
 
-p = subprocess.Popen(['python','main.py', 'key.py'])
+# p = subprocess.Popen(['python','main.py'])
+
+subprocess.run("python main.py & python key.py", shell=False)
